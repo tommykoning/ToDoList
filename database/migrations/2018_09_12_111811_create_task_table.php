c46e9bf45223d6ca177150814fce69d38b00d5c2
Tommy Koning <99039195@mydavinci.nl>
2018-09-14 10:05:15 +0200

    to do list migrations made

---
+++ database/migrations/2018_09_12_111811_create_task_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTaskTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('task_name');
+            $table->string('status');
+            $table->unsignedInteger('to_do_list_id');
+            $table->foreign('to_do_list_id')->references('id')->on('to_do_lists');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tasks');
+    }
+}
