30e1180cd15a5ac6bf8f945dc45c8871cd33e063
Tommy Koning <99039195@mydavinci.nl>
2018-10-03 09:38:48 +0200

    Update edit.blade.php

--- resources/views/toDoList/edit.blade.php
+++ resources/views/toDoList/edit.blade.php
@@ -1,7 +1,29 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: tommy
- * Date: 02/10/2018
- * Time: 11:06
- */
+@extends('layouts.app')
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">
+                        <p>create ToDoList</p>
+                        <a href="{{route('todolist.index')}}" class="btn btn-secondary">back</a>
+                    </div>
+                    <div class="card-body">
+                        <form action="{{route('todolist.update', [ 'id' => $toDoList['id']])}}" method="post">
+                            {{ csrf_field() }}
+                            {{ method_field('PUT') }}
+                            <div class="form-group">
+                                <label for="title">Title</label>
+                                <input type="text" class="form-control" id="title" name="title" placeholder="title" value="{{$toDoList['name']}}">
+                                <input type="submit">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+@endsection
+@section('script')
+
+@endsection
